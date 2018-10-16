1b96ff699c66c82bc6b6f138a4bff12da69b70eb
kitcolmx <kitcolmx@gmail.com>
2018-10-16 13:17:13 +0000

    finish chapter 2

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -17,7 +17,7 @@ public class MoneyTest
        Dollar five = new Dollar(5);
        Dollar product = five.times(2);
        assertEquals(10, product.amount);
-       Dollar product = five.times(3);
+       product = five.times(3);
        assertEquals(15, product.amount);
     }
 }
